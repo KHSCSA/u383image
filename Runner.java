bf2d2493aa58dee4f0287df37c2807fdb8a1566d
heyscottn <heyscottn@gmail.com>
2024-04-02 07:29:38 -0500

    mm

--- Runner.java
+++ Runner.java
@@ -1,13 +1,8 @@
-// sources
-// https://stackoverflow.com/questions/17174906/convert-a-bufferedimage-into-a-2d-array
-// https://dyclassroom.com/image-processing-project/how-to-get-and-set-pixel-value-in-java
-// https://dyclassroom.com/image-processing-project/how-to-convert-a-color-image-into-grayscale-image-in-java
-
 
 public class Runner{
     public static void main(String[] args){
 
-        //---------- examples
+        // --------- examples
         // each of these receive two parameters:
         // the Picture object and the output filename
         MyFunctions.speckle(new Picture("pic.jpg"), "outputSpeckle2.jpg");
