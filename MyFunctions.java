7ce93b9669e055f3e92d52c66066c29991f8304a
heyscottn <heyscottn@gmail.com>
2024-04-02 14:46:48 -0500

    mm

--- MyFunctions.java
+++ MyFunctions.java
@@ -3,9 +3,11 @@ public class MyFunctions{
     // example 1
     // this shows how we can access any pixel
     public static void speckle(Picture myPic, String filename){
+        // TODO
 
 
     }  // close speckle()
+
 
 
     // example 2
@@ -13,19 +15,24 @@ public class MyFunctions{
     // and their individual red, green, and blue values
     // then set the pixel to a specific RGB
     public static void grayscale(Picture myPic, String filename){
+        // TODO
 
 
     } // close greyscale()
+
+
 
 
     // example 3
     // this shows how we can access a portion of the image
     // and set the pixels to a specific RGB
     public static void blackBox(Picture myPic, String filename){
+        // TODO
 
 
 
     } // close blackBox()
+
 
 
 
@@ -34,6 +41,7 @@ public class MyFunctions{
     // and get the ARGB value
     // and swap with a different location
     public static void flipAcrossVertical(Picture myPic, String filename){
+        // TODO
 
 
 
@@ -44,6 +52,7 @@ public class MyFunctions{
     
     // example 5
     public static void rot180(Picture myPic, String filename){
+        // TODO
 
 
 
--- Runner.java
+++ Runner.java
@@ -1,11 +1,17 @@
 
 public class Runner{
     public static void main(String[] args){
+        // --------- exploration
+        Picture myPic = new Picture("download.jpg");
+        System.out.println(myPic.pixels[0][0]);
+        System.out.println(myPic.pixels[0][0].getValue());
+        System.out.println(myPic.pixels[0][0].getRed() + " " + myPic.pixels[0][0].getGreen() + " " + myPic.pixels[0][0].getBlue());
+
 
         // --------- examples
         // each of these receive two parameters:
         // the Picture object and the output filename
-        MyFunctions.speckle(new Picture("pic.jpg"), "outputSpeckle2.jpg");
+        // MyFunctions.speckle(new Picture("pic.jpg"), "outputSpeckle2.jpg");
         // MyFunctions.grayscale(new Picture("pic.jpg"), "outputGS.jpg");
         // MyFunctions.blackBox(new Picture("pic.jpg"), "outputBB.jpg");
         // MyFunctions.flipAcrossVertical(new Picture("pic.jpg"), "outputFlipV.jpg");
